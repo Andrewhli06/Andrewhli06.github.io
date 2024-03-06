f34b191f598f359f1cc6435e121f9f283b202d8c
Andrewhli06 <161518219+Andrewhli06@users.noreply.github.com>
2024-03-06 15:11:32 -0600

    No block detection or gravity

--- finalscene/sketch.js
+++ finalscene/sketch.js
@@ -1,26 +1,67 @@
-// Project Title
-// Your Name
-// Date
+// Geometric Mario-esque game
+// Andrew Li
+// 
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
 
-let x,y,d,dx,gy;
-let ground;
-let obstacleWidth;
+let x, y, d, dx;
+let ground, gx, gy;
+let obsWidth, obsHeight, numOb;
 let obstacles = [];
 let state = "";
 function setup() {
   createCanvas(windowWidth, windowHeight);
   state = "start";
-  ground = height/8;
-  obstacleWidth = width/40;
+  numOb = 20;
+  ground = height/10;
+  obsWidth = width/numOb;
+  obsHeight = obsWidth;
+  dx = 5;
+  gx = 0;
   gy = height - ground;
-  
-  line(0, gy, width, gy);
+  d = obsWidth;
+  x = d/2;
+  y = gy - d/2; 
 }
 
 function draw() {
   background(220);
+  line(gx, gy, width, gy);
+  randomArray();
+  blocks();
+  circle(x,y,d);
+  moveChar();
+}
+
+function blocks() {
+  for (let i = 0; i <= numOb; i++) {
+    if (obstacles[i] > 0) {
+      rect(i*obsWidth, gy-obstacles[i]*obsHeight, obsWidth, obstacles[i]*obsHeight);
+    }
+  }
+}
+
+function randomArray() {
+  for (let i = 0; i <= numOb; i++) {
+    obstacles.push(floor(random(0,6)));
+  }
+}
+
+function moveChar() {
+  if (x < d/2) {
+    x = d/2;
+  }
+  if (x > width - d/2) {
+    x = width - d/2;
+  }
+  if (x >= d / 2 && x <= width - d / 2) {
+    if (keyIsDown(65)) {
+      x -= dx;
+    }
+    if (keyIsDown(68)) {
+      x += dx;
+    }
+  }
 }
