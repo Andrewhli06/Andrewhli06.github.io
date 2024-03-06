87a1166b7d86307f5fc63cae13e657abf01255dd
Andrewhli06 <161518219+Andrewhli06@users.noreply.github.com>
2024-03-06 14:46:04 -0600

    vscode final

--- finalscene/sketch.js
+++ finalscene/sketch.js
@@ -6,8 +6,19 @@
 // - describe what you did to take this project "above and beyond"
 
 
+let x,y,d,dx,gy;
+let ground;
+let obstacleWidth;
+let obstacles = [];
+let state = "";
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  state = "start";
+  ground = height/8;
+  obstacleWidth = width/40;
+  gy = height - ground;
+  
+  line(0, gy, width, gy);
 }
 
 function draw() {
