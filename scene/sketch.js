103206f65a53f9bb6201eaa893b82f913a175d8f
Andrewhli06 <161518219+Andrewhli06@users.noreply.github.com>
2024-03-05 22:53:08 -0600

    Block detection

--- scene/sketch.js
+++ scene/sketch.js
@@ -13,6 +13,7 @@ let ground = 20;
 let d = 20;
 let dx = 1;
 let obstacles = [];
+let r = 0;
 
 let x, y, vy;
 
@@ -29,7 +30,9 @@ function draw() {
   
   let gy = height - ground;
   line(0, gy, width, gy);
-  collisionDetect();
+  xValTrack();
+  blockDetect();
+  boundaryControl();
   blocks();
   circle(x,y,d);
   
@@ -56,11 +59,13 @@ function keyPressed(){
 }
 
 function moveChar() {
-  if (keyIsDown(65)) {
-    x -= dx;
-  }
-  if (keyIsDown(68)) {
-    x += dx;
+  if (x >= d / 2 && x <= width - d / 2) {
+    if (keyIsDown(65)) {
+      x -= dx;
+    }
+    if (keyIsDown(68)) {
+      x += dx;
+    }
   }
 }
 
@@ -81,10 +86,26 @@ function randomArray() {
   }
 }
 
-function collisionDetect() {
-  let r = 0;
-  if (x === d/2 + ground*r) {
-    y = height - d/2 - obstacles[r]*ground - ground;
-    r++;
+function boundaryControl() {
+  if (x < d/2) {
+    x = d/2;
+  }
+  if (x > width - d/2) {
+    x = width - d/2;
+  }
+}
+
+function xValTrack() {
+  if ((x - d / 2) % 20 === 0) {
+    r = 1;
+  } 
+  else {
+    r = 0;
+  }
+}
+
+function blockDetect() {
+  if (r === 1) {
+    y = height - ground - d / 2 - obstacles[(x - d / 2) / 20] * ground;
   }
 }
