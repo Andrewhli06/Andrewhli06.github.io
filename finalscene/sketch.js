40d7838d9803becb4f990ece3adc0c6cfbadeb64
Andrewhli06 <161518219+Andrewhli06@users.noreply.github.com>
2024-03-08 18:17:26 -0600

    collision detection implementation

--- finalscene/sketch.js
+++ finalscene/sketch.js
@@ -5,17 +5,26 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let x,y,d;
-let ground, gy;
-let state, stateChar;
-let obstacles = [];
+let x,y,d; // circle parameters
+let dx, dy, g, jump; // physics parameters
+let rx, ry; // rectangle parameters
+let ground, gy; // ground parameters
+let state, stateChar; // various states
+let obstacles = []; // array generating obstacles
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  x = width/2;
-  y = height/2;
+  rx = width/2;
+  ry = height/2;
   ground = height/10;
   gy = height - ground;
+  dx = 5;
+  dy = 0;
+  g = 1;
+  jump = 10;
+  d = 20;
+  x = d/2;
+  y = gy - d/2;
 }
 
 function windowResized() {
@@ -25,20 +34,76 @@ function windowResized() {
 function draw() {
   background(220);
   line(0, gy, width, gy);
-  rect(x, y, 60, y-ground);
-  circle(mouseX, mouseY, 20);
+  rect(rx, ry, 60, ry-ground);
+  circle(x, y, d);
   obstacleDetect();
+  moveChar();
+  obstacleParameters();
+  console.log(y);
 }
 
 function obstacleDetect() {
-  if (collideLineCircle(0, gy, width, gy, mouseX, mouseY, 20)) {
-    stateChar = "ground";
+  if (collideRectCircle(rx, ry, 60, ry-ground, x, y, d) && y === gy - d /2) {
+    stateChar = "groundBlock";
   }
-  else if (collideRectCircle(x, y, 60, y-ground, mouseX, mouseY, 20)) {
-    stateChar = "block";
+  else if (collideRectCircle(rx, ry, 60, ry-ground, x, y, d) && y === ry - d/2)  {
+    stateChar = "blockTop";
+  }
+  else if (collideRectCircle(rx, ry, 60, ry-ground, x, y, d) && (y !== gy - d /2 && y !== ry - d/2) && y < ry - d/2) {
+    stateChar = "blockSide";
+  }
+  else if (collideLineCircle(0, gy, width, gy, x, y, d)) {
+    stateChar = "ground";
   }
   else {
     stateChar = "air";
   }
   console.log(stateChar);
 }
+
+function moveChar() {
+  y += dy;
+  if ((stateChar === "groundBlock" || stateChar === "blockSide") && y < ry - ground) {
+    x = rx - d/2 - dx;
+  }
+  else if (x < d/2) {
+    x = d/2 + dx;
+  }
+  else if (x > width - d/2) {
+    x = width - d/2 - dx;
+  }
+  else if (x >= d / 2 && x <= width - d / 2) {
+    if (keyIsDown(65)) {
+      x -= dx;
+    }
+    if (keyIsDown(68)) {
+      x += dx;
+    }
+  }
+}
+
+function obstacleParameters() {
+  if (stateChar === "groundBlock") {
+    dy = 0;
+    y = ry - d/2;
+  }
+  if (stateChar === "air") {
+    dy += g;
+    if (y > ry - d/2) {
+      dy = 0;
+      y = ry - d/2;
+    }
+    if (y > gy - d/2) {
+      dy = 0;
+      y = gy - d/2;
+    }
+  }
+}
+
+// function keyPressed(){
+//   if (key === " ") {
+//     if(stateChar === "block" || state === "ground"){ // on the ground
+//       dy = -jump; 
+//     }
+//   }
+// }
