aef0443aca11b7ea846ac9fafcd5a9532dfdab3e
Andrewhli06 <161518219+Andrewhli06@users.noreply.github.com>
2024-03-27 16:10:16 -0600

    idk

--- gridshot/sketch.js
+++ gridshot/sketch.js
@@ -4,12 +4,57 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-
+let theTargets = [];
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  noStroke();
+  cursor(CROSS);
+  spawnTargets();
+
+  window.setInterval(spawnTargets, 500);
+
+  window.setTimeout(deleteTargets, 1000);
 }
 
 function draw() {
   background(220);
+  generateGrid();
+}
+
+function generateGrid() {
+  ambientMaterial(255);
+  directionalLight(theTargets[0].r, theTargets[0].g, theTargets[0].b, theTargets[0].lightX, theTargets[0].lightY, theTargets[0].lightZ);
+  translate(theTargets[0].transX, theTargets[0].transY);
+  sphere(theTargets[0].size);
+  for (let i = 1; i < theTargets.length; i++) {
+    ambientMaterial(255);
+    translate(theTargets[i].transX, theTargets[i].transY);
+    sphere(theTargets[i].size);
+  }
+}
+
+function spawnTargets() {
+  let someTarget = {
+    size: 20,
+    transX: random(-50, 50),
+    transY: random(-50, 50),
+    // speed: 3,
+    r: 0,
+    g: 255,
+    b: 255,
+    lightX: 0,
+    lightY: 0,
+    lightZ: -100,
+    timeX: random(1000000),
+    timeY: random(1000000),
+    dt: 0.001,
+  };
+  theTargets.push(someTarget);
+}
+
+function deleteTargets() {
+  for (let i = 1; i < theTargets.length; i++) {
+    theTargets.splice(i, 1);
+  }
 }
