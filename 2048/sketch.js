04890f73521d68cc42db9039ff1cc20b904e3597
Andrewhli06 <161518219+Andrewhli06@users.noreply.github.com>
2024-04-25 23:31:37 -0600

    basic left and right combination and movement

--- 2048/sketch.js
+++ 2048/sketch.js
@@ -25,6 +25,10 @@ function setup() {
   grid = generateGrid(GRID_SIZE, GRID_SIZE);
 
   cellSize = height/grid.length;
+
+  for (let i = 0; i < 2; i++) {
+    spawnCells();
+  }
 }
 
 function draw() {
@@ -42,8 +46,26 @@ function displayGrid() {
         stroke("grey");
         square(x * cellSize, y * cellSize, cellSize, CORNER_SIZE);
       }
-      else if (grid[y][x] === 1) {
+      else if (grid[y][x] === 2) {
         fill("black");
+        strokeWeight(CORNER_SIZE);
+        stroke("grey");
+        square(x * cellSize, y * cellSize, cellSize, CORNER_SIZE);
+      }
+      else if (grid[y][x] === 4) {
+        fill("red");
+        strokeWeight(CORNER_SIZE);
+        stroke("grey");
+        square(x * cellSize, y * cellSize, cellSize, CORNER_SIZE);
+      }
+      else if (grid[y][x] === 8) {
+        fill("blue");
+        strokeWeight(CORNER_SIZE);
+        stroke("grey");
+        square(x * cellSize, y * cellSize, cellSize, CORNER_SIZE);
+      }
+      else if (grid[y][x] === 16) {
+        fill("green");
         strokeWeight(CORNER_SIZE);
         stroke("grey");
         square(x * cellSize, y * cellSize, cellSize, CORNER_SIZE);
@@ -57,14 +79,92 @@ function generateGrid(columns, rows) {
   for (let y = 0; y < rows; y++) {
     emptyArray.push([]);
     for (let x = 0; x < columns; x++) {
-      if (random(10) > 9) { //change this later to guarantee 2 cells spawning everytime
-        emptyArray[y].push(1);
-      }
-      else {
-        emptyArray[y].push(OPEN_CELL);
-      }
+      emptyArray[y].push(OPEN_CELL);
     }
   }
   return emptyArray;
 }
 
+function spawnCells() {
+  let openCells = [];
+  for (let i = 0; i < GRID_SIZE; i++) {
+    for (let j = 0; j < GRID_SIZE; j++) {
+      if (grid[i][j] === 0) {
+        openCells.push({
+          x: i,
+          y: j,
+        });
+      }
+    }
+  }
+  if (openCells.length > 0) {
+    let spot = random(openCells);
+    if (random(1) > 0.5) {
+      grid[spot.x][spot.y] = 2;
+    }
+    else {
+      grid[spot.x][spot.y] = 4;
+    }
+  }
+}
+
+function notEmpty(value) {
+  return value > 0; //segregates none zero values, that is to say cells that are filled
+}
+
+function moveCellsLeft(row) {
+  let newRow = row.filter(notEmpty); // makes an array with only non empty values
+  let missing = GRID_SIZE - newRow.length; // calculates how many zeros are needed to fill the empty spots
+  let zeros = Array(missing).fill(0); // creates array with sufficient zeros to fill the gap
+  newRow = newRow.concat(zeros); // makes the array that pushes all non empty blocks to the left
+  return newRow;
+}
+
+function moveCellsRight(row) {
+  let newRow = row.filter(notEmpty); // makes an array with only non empty values
+  let missing = GRID_SIZE - newRow.length; // calculates how many zeros are needed to fill the empty spots
+  let zeros = Array(missing).fill(0); // creates array with sufficient zeros to fill the gap
+  newRow = zeros.concat(newRow); // makes the array that pushes all non empty blocks to the right
+  return newRow;
+}
+
+function keyPressed() {
+  if (key === "a") {
+    for (let i = 0; i < GRID_SIZE; i++) {
+      grid[i] = moveCellsLeft(grid[i]);
+      combineCellsLeft(grid[i]);
+    }
+    spawnCells();
+  }
+  else if (key === "d") {
+    for (let i = 0; i < GRID_SIZE; i++) {
+      grid[i] = moveCellsRight(grid[i]);
+      combineCellsRight(grid[i]);
+    }
+    spawnCells();
+  }
+}
+
+function combineCellsRight(row) {
+  for (let i = row.length - 1; i >= 1; i--) {
+    let first = row[i];
+    let second = row[i-1];
+    if (first === second) {
+      row[i] = first + second;
+      row[i-1] = 0;
+      break;
+    }
+  }
+}
+
+function combineCellsLeft(row) {
+  for (let i = 0; i < row.length-1; i++) {
+    let first = row[i];
+    let second = row[i+1];
+    if (first === second) {
+      row[i] = first + second;
+      row[i+1] = 0;
+      break;
+    }
+  }
+}
