12414767760602ef108314b6d9a2015875b41462
Andrewhli06 <161518219+Andrewhli06@users.noreply.github.com>
2024-04-30 00:58:16 -0600

    commenting

--- 2048/sketch.js
+++ 2048/sketch.js
@@ -7,6 +7,7 @@
 // - I also explored the functionality of a ternary operator in place of a single if else statement
 // - I also explored the use of many different array modifying operators such as fill, filter, concat, etc.
 
+//initializing global variables and constants
 let cellSize;
 let grid;
 let score = 0;
@@ -21,7 +22,7 @@ function setup() {
 
   cellSize = height/grid.length;
 
-  for (let i = 0; i < 2; i++) {
+  for (let i = 0; i < 2; i++) { //randomly spawns two cells upon start
     spawnCells();
   }
 }
@@ -33,11 +34,12 @@ function draw() {
   displayScore();
 }
 
+//displays the 4x4 grid, allocates appropriate colour to respective cell
 function displayGrid() {
   for (let y = 0; y < grid.length; y++) {
     for (let x = 0; x < grid[y].length; x++) {
       switch(grid[y][x]) {
-      case 0:
+      case OPEN_CELL:
         cellColour("#a9a9a9");
         square(x * cellSize, y * cellSize, cellSize, CORNER_SIZE);
         break;
@@ -98,6 +100,7 @@ function displayGrid() {
   }
 }
 
+//generates the 2D array responsible for the creation of the grid
 function generateGrid(columns, rows) {
   let emptyArray = [];
   for (let y = 0; y < rows; y++) {
@@ -109,11 +112,12 @@ function generateGrid(columns, rows) {
   return emptyArray;
 }
 
+//spawns cells in random location
 function spawnCells() {
   let openCells = [];
   for (let i = 0; i < GRID_SIZE; i++) {
     for (let j = 0; j < GRID_SIZE; j++) {
-      if (grid[i][j] === 0) {
+      if (grid[i][j] === 0) { //assigns an x and y coordinate to each cell in the grid
         openCells.push({
           x: i,
           y: j,
@@ -123,23 +127,25 @@ function spawnCells() {
   }
   if (openCells.length > 0) {
     let spot = random(openCells);
-    random(1) > 0.5 ? grid[spot.x][spot.y] = 2 : grid[spot.x][spot.y] = 4;
+    random(1) > 0.5 ? grid[spot.x][spot.y] = 2 : grid[spot.x][spot.y] = 4; //chooses to randomly spawn either a 2 or a 4
   }
 }
 
+//gives colour to the cell, forms the border of the grid
 function cellColour(colour) {
   fill(colour);
   strokeWeight(CORNER_SIZE);
   stroke("grey");
 }
 
+//segregates non-zero values
 function notEmpty(value) {
-  return value > 0; //segregates non-zero values, that is to say cells that are filled
+  return value > OPEN_CELL; 
 }
 
 function moveCellsLeft(row) {
   let newRow = row.filter(notEmpty); // makes an array with only non empty values
-  let empty = GRID_SIZE - newRow.length; // calculates how many zeros are needed to fill the empty spots
+  let empty = GRID_SIZE - newRow.length; 
   let zeros = Array(empty).fill(0); // creates array with sufficient zeros to fill the gap
   newRow = newRow.concat(zeros); // makes the array that pushes all non empty blocks to the left
   return newRow;
@@ -147,13 +153,14 @@ function moveCellsLeft(row) {
 
 function moveCellsRight(row) { 
   let newRow = row.filter(notEmpty); // makes an array with only non empty values
-  let empty = GRID_SIZE - newRow.length; // calculates how many zeros are needed to fill the empty spots
+  let empty = GRID_SIZE - newRow.length;
   let zeros = Array(empty).fill(0); // creates array with sufficient zeros to fill the gap
   newRow = zeros.concat(newRow); // makes the array that pushes all non empty blocks to the right
   return newRow;
 }
 
-function rotateGrid() { //this function
+//turns the columns into rows in order to recycle the right and left movement/combine functions
+function rotateGrid() { 
   let newGrid = [];
   for (let i = 0; i < GRID_SIZE; i++) {
     newGrid.push([]);
@@ -181,7 +188,7 @@ function keyPressed() {
     }
     spawnCells();
   }
-  else if (key === "w" || keyCode === UP_ARROW) { // rotates the grid to turn cols to rows, makes the modification, then returns the rows to cols
+  else if (key === "w" || keyCode === UP_ARROW) {
     grid = rotateGrid();
     for (let i = 0; i < GRID_SIZE; i++) {
       grid[i] = moveCellsLeft(grid[i]);
@@ -191,7 +198,7 @@ function keyPressed() {
     grid = rotateGrid();
     spawnCells();
   }
-  else if (key === "s" || keyCode === DOWN_ARROW) { // rotates the grid to turn cols to rows, makes the modification, then returns the rows to cols
+  else if (key === "s" || keyCode === DOWN_ARROW) {
     grid = rotateGrid();
     for (let i = 0; i < GRID_SIZE; i++) {
       grid[i] = moveCellsRight(grid[i]);
@@ -203,34 +210,37 @@ function keyPressed() {
   }
 }
 
-function combineCellsRight(row) {
+//checks if neighbouring cells are the same, if so, add them up and replace old location with an empty cell
+function combineCellsRight(row) { 
   for (let i = row.length - 1; i >= 1; i--) {
     let first = row[i];
     let second = row[i-1];
     if (first === second) {
       row[i] = first + second;
-      row[i-1] = 0;
+      row[i-1] = OPEN_CELL;
       score = score + (first + second);
     }
   }
 }
 
+//checks if neighbouring cells are the same, if so, add them up and replace old location with an empty cell
 function combineCellsLeft(row) {
   for (let i = 0; i < row.length-1; i++) {
     let first = row[i];
     let second = row[i+1];
     if (first === second) {
       row[i] = first + second;
-      row[i+1] = 0;
+      row[i+1] = OPEN_CELL;
       score = score + (first + second);
     }
   }
 }
 
+//shows the value of the cell, places the text in the middle of said cell
 function cellText() {
   for (let i = 0; i < GRID_SIZE; i++) {
     for (let j = 0; j < GRID_SIZE; j++) {
-      if (grid[i][j] > 0) {
+      if (grid[i][j] > OPEN_CELL) {
         fill("black");
         noStroke();
         textSize(42);
@@ -241,6 +251,7 @@ function cellText() {
   }
 }
 
+//keeps a running total for the sum of the combined blocks
 function displayScore() {
   fill("black");
   textSize(42);
