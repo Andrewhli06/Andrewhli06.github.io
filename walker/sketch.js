37e2f43350d28882c9e30f4c75c27286913add6a
Andrewhli06 <161518219+Andrewhli06@users.noreply.github.com>
2024-04-30 20:06:56 -0600

    idk

--- walker/sketch.js
+++ walker/sketch.js
@@ -1,15 +1,48 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+//walker demo
 
+class Walker {
+  constructor(x, y, color) {
+    this.x = x;
+    this.y = y;
+    this.stepSize = 5;
+    this.color = color;
+    this.radius = 5;
+  }
+  display() {
+    fill(this.color);
+    circle(this.x, this.y, this.radius*2);
+  }
+  move() {
+    let choice = random(100);
+    if (choice < 25) {
+      this.y -= this.stepSize;
+    }
+    else if (choice < 50) {
+      this.y += this.stepSize;
+    }
+    else if (choice < 75) {
+      this.x += this.stepSize;
+    }
+    else {
+      this.x -= this.stepSize;
+    }
+  }
+}
+
+let character;
+let char2;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  character = new Walker(width/2, height/2, "red");
+  char2 = new Walker(width/4, height/4, "blue");
+  noStroke();
+  
 }
 
 function draw() {
-  background(220);
+  character.move();
+  char2.move();
+  character.display();
+  char2.display();
 }
