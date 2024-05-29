d5c14cdedd2623b2145b8c1c4a2c8ca65e2879b4
Andrewhli06 <161518219+Andrewhli06@users.noreply.github.com>
2024-05-29 13:22:16 -0600

    recursive circle

--- recCircles/sketch.js
+++ recCircles/sketch.js
@@ -12,4 +12,16 @@ function setup() {
 
 function draw() {
   background(220);
+  recursiveCircle(width/2, height/2, mouseX)
+}
+
+function recursiveCircle(x, y, r) {
+  circle(x, y, r*2)
+
+  if (r > 30) {
+    recursiveCircle(x - r/2, y, r/2)
+    recursiveCircle(x + r/2, y, r/2)
+    recursiveCircle(x, y - r/2, r/2)
+    recursiveCircle(x, y + r/2, r/2)
+  } 
 }
