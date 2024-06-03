a287696740c0ace3985bfe198f25acca69ea7e4c
Andrewhli06 <161518219+Andrewhli06@users.noreply.github.com>
2024-06-03 13:37:25 -0600

    relatively successful bullet detection

--- mpTesting/sketch.js
+++ mpTesting/sketch.js
@@ -1,53 +1,3 @@
-class Player {
-  constructor(x, y, transX, transY, dx, dy, size) {
-    this.position = createVector(x, y);
-    this.transX = transX;
-    this.transY = transY;
-    this.dx = dx;
-    this.dy = dy;
-    this.size = size;
-    this.rotateX = mouseX - this.transX;
-    this.rotateY = mouseY - this.transY;
-    this.rotateAngle = atan2(this.rotateY, this.rotateX);
-  }
-  display() {
-    push();
-    translate(this.transX, this.transY);
-    rect(this.position.x, this.position.y, this.size*2, this.size*2);
-    pop();
-    push();
-    fill("red");
-    translate(this.transX, this.transY);
-    rotate(this.rotateAngle);
-    rect(this.position.x, this.position.y, this.size, this.size);
-    pop();
-  }
-  pRotate() {
-    this.rotateX = mouseX - this.transX;
-    this.rotateY = mouseY - this.transY;
-    this.rotateAngle = atan2(this.rotateY, this.rotateX);
-  }
-  move() {
-    if (keyIsDown(87)) {
-      this.transY -= this.dy;
-    }
-    else if (keyIsDown(83)) {
-      this.transY += this.dy;
-    }
-    else if (keyIsDown(65)) {
-      this.transX -= this.dx;
-    }
-    else if (keyIsDown(68)) {
-      this.transX += this.dx;
-    }
-  }
-  update() {
-    character.move();
-    character.pRotate()
-    character.display()
-  } 
-}
-
 class Bullet {
   constructor(x, y, transX, transY, dx, dy, size) { 
     this.position = createVector(x, y);
@@ -56,89 +6,60 @@ class Bullet {
     this.dx = dx;
     this.dy = dy;
     this.size = size;
-    this.rotateX = mouseX - this.transX;
-    this.rotateY = mouseY - this.transY;
-    this.rotateAngle = atan2(this.rotateY, this.rotateX) - 90;
+    // this.rotateX = mouseX - this.transX;
+    // this.rotateY = mouseY - this.transY;
+    // this.rotateAngle = atan2(this.rotateY, this.rotateX) - 90;
   }
   display() {
     push();
     translate(this.transX, this.transY);
-    rotate(this.rotateAngle);
+    //rotate(this.rotateAngle);
     fill("black");
     circle(this.position.x, this.position.y, this.size);
     pop();
   }
   move() {
-    this.position.y += this.dy;
+    this.transY += this.dy;
   }
 }
 
-
-let character;
 let bullets = [];
 let transX, transY;
 let dx, dy;
 function setup() {
-  angleMode(DEGREES);
-  rectMode(CENTER);
+  rectMode(CORNER)
+  createCanvas(400, 400);
   dx = 1;
   dy = 1;
-  createCanvas(400, 400);
   transX = width/2
   transY = height/2
-  character = new Player(0, 0, transX, transY, dx, dy, 20);
 }
 
 function draw() {
   background(220);
-  character.update();
-  wall();
   for (let bullet of bullets) {
     bullet.move();
     bullet.display();
   }
+  wall();
   bulletDetection();
-  barrierDetection();
-  movement();
 }
 
 function mousePressed() {
   bullets.push(new Bullet(0, 0, transX, transY, dx, dy, 5));
 }
 
-function movement() {
-  if (keyIsDown(87)) {
-    transY -= dy;
-  }
-  else if (keyIsDown(83)) {
-    transY += dy;
-  }
-  else if (keyIsDown(65)) {
-    transX -= dx;
-  }
-  else if (keyIsDown(68)) {
-    transX += dx;
-  }
-}
-
 function wall() {
-  rect(width - 25, height/2, 50, 100);
+  rect(0, height-25, width, 25)
 }
 
 function bulletDetection() {
   for (let bullet of bullets) {
-    if (collideRectCircle(width - 50, height/2 - 50, 50, 100, bullet.position.x, bullet.position.y, bullet.size)) {
+    if (collideRectCircle(0, height-25, width, 25, bullet.transX, bullet.transY, bullet.size)) {
       console.log("hit");
+      bullet.dy = 0;
     }
+    console.log(bullet.transX, bullet.transY);
   }
 
-}
-
-function barrierDetection() {
-  if (collideRectRect(character.transX - character.size, character.transY - character.size, character.size*2, character.size*2, width - 50, height/2 - 50, 50, 100)) {
-    console.log("wall");
-  }
-  else {
-    console.log("empty");
-  }
 }
